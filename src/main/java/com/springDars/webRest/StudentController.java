8de77466f74e9024301d147401ae8456a1890712
Ergashboy <example@gmail.com>
2021-05-23 20:17:32 +0500

    StudentController.java da ozgarish

--- src/main/java/com/springDars/webRest/StudentController.java
+++ src/main/java/com/springDars/webRest/StudentController.java
@@ -1,4 +1,14 @@
 package com.springDars.webRest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class StudentController {
+    @GetMapping("/student")
+    public String hello(){
+        return "Salom";
+    }
 }
