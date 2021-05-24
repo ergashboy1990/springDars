2b436aa55fe2e6245ea04eab26a26f9bc8fb34da
ergashboy1990 <84660405+ergashboy1990@users.noreply.github.com>
2021-05-24 08:28:10 +0300

    api/talabalar yaratildi

---
+++ src/main/java/com/springDars/SpringBirinchiDarsApplication.java
@@ -0,0 +1,13 @@
+package com.springDars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBirinchiDarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBirinchiDarsApplication.class, args);
+	}
+
+}
